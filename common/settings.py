fea9ecd8d6db69699ae3871fa13d6b390f7bab01
Rooman Abdul Latif <Rooman.AbdulLatif@imperiumdynamics.com>
2025-11-06 12:17:32 +0500

    Implemented msal

---
+++ common/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+from functools import lru_cache
+class Settings(BaseSettings):
+    TENANT_ID: str = Field(..., env="TENANT_ID")
+    CLIENT_ID: str = Field(..., env="CLIENT_ID")
+    CLIENT_SECRET: str = Field(..., env="CLIENT_SECRET")
+    GRAPH_BASE: str = "https://graph.microsoft.com/v1.0"
+    SHAREPOINT_HOSTNAME: str = Field(..., env="SHAREPOINT_HOSTNAME")
+    SITE_PATH: str = Field(..., env="SITE_PATH")
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
