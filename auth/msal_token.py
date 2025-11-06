1360be87d87a62b8f910715e96e8c54ed39ad403
Rooman Abdul Latif <Rooman.AbdulLatif@imperiumdynamics.com>
2025-11-06 12:17:32 +0500

    Implemented msal

---
+++ auth/msal_token.py
@@ -0,0 +1,18 @@
+from common.settings import get_settings
+import msal
+
+_settings = get_settings()
+
+def get_token() -> str:
+    app = msal.ConfidentialClientApplication(
+        _settings.CLIENT_ID,
+        authority=f"https://login.microsoftonline.com/{_settings.TENANT_ID}",
+        client_credential=_settings.CLIENT_SECRET
+    )
+    scope = ["https://graph.microsoft.com/.default"]
+    result = app.acquire_token_silent(scope, account=None)
+    if not result:
+        result = app.acquire_token_for_client(scopes=scope)
+    if "access_token" not in result:
+        raise Exception(f"Could not obtain access token: {result}")
+    return result["access_token"]
---
+++ main.py
@@ -0,0 +1,4 @@
+from auth.msal_token import get_token
+
+app=get_token()
+print(app)
