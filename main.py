5b2d94bf0426109003c4548cc19d358cedc96548
Ahmed Mustafa <ahmed.mustafa@imperiumdynamics.com>
2025-11-11 17:26:45 +0500

    merged from rooman

--- main.py
+++
@@ -1,7 +0,0 @@
-
-from services.download.download_file import download_file_from_root
-from common.settings import get_settings
-_settings = get_settings()
-if _settings.FILE_NAME:
-    saved = download_file_from_root(file_path=_settings.FILE_NAME)
-    print(f"Downloaded to: {saved}")
